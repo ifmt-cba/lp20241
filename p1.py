1f39ffbb6f857a8759def4a7bae5084c67290201
preti-joao <108187064+preti-joao@users.noreply.github.com>
2024-04-03 18:46:39 -0400

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,32 @@
+'''
+PROVA PRÁTICA DE PYTHON
+Ao término enviar e-mail conforme modelo:
+Para:       preti.joao@ifmt.edu.br
+Assunto:    Prova 1 de Laboratório de Programação 2024/1
+Mensagem:   NOME COMPLETO DO ESTUDANTE
+Anexo:      p1.py
+'''
+
+#1. Faça um programa que leia o valor unitário de um produto,
+#   a quantidade desejada e imprima o valor total a pagar. (2,5pt)
+
+#2. Faça um programa que permita entrar com o nome, a nota da prova 1 e a nota
+#   da prova 2 de um aluno. O programa deve imprimir o nome, a nota da prova 1,
+#   a nota da prova 2, a média das notas e uma das mensagens: "Aprovado",
+#   "Reprovado" ou "em Prova Final"(a média é 7 para aprovação, menor que 3 para
+#   reprovação e as demais em prova final). (2,5pt)
+
+#3. Faça um programa que apresente um menu com 4 opções:
+#   [1] - Cadastrar
+#   [2] - Excluir
+#   [3] - Pesquisar
+#   [0] - Sair
+#   O usuário deve escolher uma opção e o programa deve imprimir uma mensagem 
+#   ao entrar em cada opção do menu. O programa só deve encerrar quando a opção
+#   escolhida for zero (0). (2,5pt)
+
+#4. Faça um programa que calcule o retorno de um investimento. (2,5pt)
+#   O programa deve solicitar do usuário o:
+#   - valor que será investido;
+#   - prazo do investimento (meses);
+#   - juros mensal (juros composto).
